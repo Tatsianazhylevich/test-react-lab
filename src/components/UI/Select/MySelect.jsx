65d99858aae6bc82fc417cfaecfa7b67a30a25a5
tatsiana zhylevich <tatzhylevich@gmail.com>
2021-09-18 17:50:33 +0300

    add: select by state

---
+++ src/components/UI/Select/MySelect.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MySelect = ({ defaultValue, value, filteredPosts, selectByState }) => {
+    return (
+        <StyledSelect value={value} onChange={selectByState}>
+          <option disabled value="">{defaultValue}</option>
+          <option value="">All</option>
+          {filteredPosts.map((data) => 
+            <option key={data.phone} value={data.adress.state}>{data.adress.state}</option>
+            )}
+        </StyledSelect>
+    )
+}
+
+export default MySelect;
+
+const StyledSelect = styled.select`
+    margin-top: 15px;
+    border: 1px solid teal;
+`
